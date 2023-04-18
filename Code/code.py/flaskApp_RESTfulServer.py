0e546189786da1929c34fcd085cbd12f1ce84e07
Todd Gavin <tgavin@usc.edu>
2023-04-17 17:04:13 -0700

    Curl commands working
    # TODO:
    # 1/ Error handling for when request methods are incorrect, data is incorrect
    # 2/ Print out data whenever any write function occurs
    # 3/ Create filter functions
    # 4/ Create streamlit GUI for CLI

---
+++ Code/code.py/flaskApp_RESTfulServer.py
@@ -0,0 +1,104 @@
+# TODO:
+# 1/ Error handling for when request methods are incorrect, data is incorrect
+# 2/ Print out data whenever any write function occurs
+# 3/ Create filter functions
+# 4/ Create streamlit GUI for CLI
+
+
+from mongoDB_driver import connectMongoDB, db_collection_document, get, put, post, patch, delete
+
+from flask import Flask, request, jsonify
+
+# Build flask app
+app = Flask(__name__)
+
+# Create MongoDB client
+client = connectMongoDB()
+
+# Establish the database, collection, and document that data will be read/written from/to
+db, collection, documentFilter = db_collection_document(client, 'firebaseRealtimeDatabase', 'firebaseCollection', '643c926ac5712e741b10398a')
+# print(f"db: {db}\ncollection: {collection}\ndocumentFilter:{documentFilter}")
+
+# Set request content-type to application/json 
+@app.before_request
+def before_request():
+    if request.method in ['POST', 'PATCH', 'PUT']:
+        request.environ['CONTENT_TYPE'] = 'application/json'
+
+# Request handler function
+@app.route('/', defaults={'path': ''}, methods=['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
+@app.route('/<path:path>', methods=['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
+def handle_request(path):
+
+    # Acces global variables to connect to MongoDB
+    global collection
+    global documentFilter
+    # print(f"Log:\ncollection: {collection}\n\ndocumentFilter:{documentFilter}\n\n")
+
+    # Get the path paramters to define where to read/write data from/to
+    path_params = path.split('/')
+    path_params = [string for string in path_params if string != ""]
+    print(f"Log: path_params = {path_params}")
+
+    # Create the JSON path using the path paramaters
+    if len(path_params) == 1:
+        jsonPath = path_params[0]
+    else:
+        jsonPath = ".".join(path_params)
+    print(f"Log: jsonPath = {jsonPath}")
+
+    # Get the data from the CURL command
+    data = request.get_json()
+
+    # Check the request method, execute respective function to sed request method
+
+    # curl -X GET 'http://127.0.0.1:5000/'
+    # curl -X GET 'http://127.0.0.1:5000/users/'
+    if request.method == 'GET':
+        get_result = get(collection, documentFilter, jsonPath, filter=None)[0]
+        print("Log: GET Executed")
+        print(f"Log: get_result = {get_result}")
+        return str(get_result)
+    
+    # curl -X POST 'http://127.0.0.1:5000/users/' -d '{"106": {"name": "Amanda", "age": 22}}'
+    elif request.method == 'POST':
+        if data:
+            post(collection, documentFilter, jsonPath, data)
+            print(f'Log: POST request received with data: {data}')
+        else:
+            print('Log: POST request received without data')
+        print("Log: POST Executed")
+        return 'POST request received'
+    
+    # curl -X PUT 'http://127.0.0.1:5000/users/' -d '{"105": {"name": "Amanda", "age": 22}}' 
+    elif request.method == 'PUT':
+        if data:
+            put(collection, documentFilter, jsonPath, data)
+            print(f'Log: PUT request received with data: {data}')
+        else:
+            print('Log: PUT request received without data')
+        print("Log: PUT Executed")
+        return 'PUT request received'
+    
+    # curl -X PATCH 'http://127.0.0.1:5000/users/105/' -d '{"age": 26}' 
+    elif request.method == "PATCH":
+        if data:
+            patch(collection, documentFilter, jsonPath, data)
+            print(f'Log: PATCH request received with data: {data}')
+        else:
+            print('Log: PATCH request received without data')
+        print("Log: PATCH Executed")
+        return 'PATCH request received'
+    
+    # curl -X DELETE 'http://127.0.0.1:5000/users/105/'
+    elif request.method == 'DELETE':
+        delete(collection, documentFilter, jsonPath)
+        print("Log: DELETE Executed")
+        return 'DELETE request received'
+    
+    else:
+        return 'Invalid request'
+
+if __name__ == '__main__':
+
+    app.run(port=5000)
---
+++ Code/code.py/mongoDB_driver.py
@@ -0,0 +1,153 @@
+import certifi
+import pymongo
+from bson.objectid import ObjectId
+import json
+
+##########################################################
+# CONNECT TO MONGODB w/ DRIVER
+##########################################################
+def connectMongoDB():
+    ca = certifi.where()
+    username = "toddgavin"
+    password = "XGiMOhZ1XMCpxMJX"
+    uri = f"mongodb+srv://{username}:{password}@cluster0.u0ixrbx.mongodb.net/?retryWrites=true&w=majority"
+
+    client = pymongo.MongoClient(uri, tlsCAFile=ca)
+    db = client.test
+
+    try:
+        client.admin.command('ping')
+        print("Log: Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        print("Error connecting.")
+        print(e)
+
+    return client
+
+##########################################################
+# DEFINE DB, COLLECTION, AND DOCUMENT FILTER\
+##########################################################
+
+def db_collection_document(client, db_name, collection_name, objectId):
+
+    # get a reference to the database
+    db = client[db_name]
+
+    # get a reference to the collection
+    collection = db[collection_name]
+
+    # define the filter to match the document with a specific _id
+    documentFilter = {'_id': ObjectId(objectId)}
+
+    return db, collection, documentFilter
+
+#############################
+# GET
+#############################
+def helper_filter(orderBy=None, limit=None, equalTo=None, startAt=None, endAt=None):
+    filter = {}
+    
+    if equalTo is not None:
+        filter['$eq'] = equalTo
+        
+    if startAt is not None:
+        filter['$gte'] = startAt
+        
+    if endAt is not None:
+        filter['$lte'] = endAt
+        
+    if orderBy is not None:
+        filter['$sort'] = {orderBy: 1}
+        
+    if limit is not None:
+        filter['$limit'] = limit
+        
+    return filter
+
+def get(collection, documentFilter, jsonPath, filter=None):
+    if filter is None:
+        if jsonPath == '':
+            result = list(collection.find(documentFilter))
+        else: 
+            result = collection.find(documentFilter).distinct(jsonPath)
+    else:
+        filter = helper_filter(**filter)
+        result = collection.find({'$and': [documentFilter, filter]}).distinct(jsonPath)
+    return result
+
+
+#############################
+# PUT
+#############################
+def helper_Update_PUT(data, jsonPath):
+
+    updates = {}
+
+    for key, value in data.items():
+        if jsonPath == '':
+            newKey = f"{key}"
+        else:
+            newKey = f"{jsonPath}.{key}"
+        updates[newKey] = value
+
+    update = {'$set': updates}
+    # print(update)
+    return update
+
+def put(collection, documentFilter, jsonPath, data):
+    update = helper_Update_PUT(data, jsonPath)
+    # print(update)
+    collection.update_one(documentFilter, update)
+
+#############################
+# POST
+#############################
+def helper_Update_POST(data, jsonPath):
+
+    updates = {}
+    unique_key = str(ObjectId())
+
+    if jsonPath == '':
+        jsonPath = f"{unique_key}"
+    else:
+        jsonPath = f"{jsonPath}.{unique_key}"
+
+    for key, value in data.items():
+        newKey = f"{jsonPath}.{key}"
+        updates[newKey] = value
+
+    # uniqueKeyUpdates = {}
+    # uniqueKeyUpdates[unique_key] = updates
+
+    update = {'$set': updates}
+    print(update)
+    return update
+
+def post(collection, documentFilter, jsonPath, data):
+    update = helper_Update_POST(data, jsonPath)
+    collection.update_one(documentFilter, update)
+
+#############################
+# PATCH
+#############################
+
+# PATCH either writes data if it doesnt exist or rewrites the data if it does exist
+def patch(collection, documentFilter, jsonPath, data):
+    # get the current json within the jsonPath and rewrite/add the data
+
+    currentData = get(collection, documentFilter, jsonPath, None)[0]
+    merged_data = currentData.copy()
+    merged_data.update(data)
+
+    print(currentData)
+    print(data)
+    print(merged_data)
+
+    put(collection, documentFilter, jsonPath, merged_data)
+
+#############################
+# DELETE
+#############################
+def delete(collection, documentFilter, jsonPath):
+    update = {'$unset': {jsonPath: 1}}
+    collection.update_one(documentFilter, update)
